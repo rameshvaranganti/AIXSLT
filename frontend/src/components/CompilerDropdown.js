0d30f0fe4bd146cc620b7113cbc9dd803827adce
rameshvaranganti <ramesh.varanganti@gmail.com>
2025-12-02 20:02:38 -0500

    Add CompilerDropdown component

---
+++ frontend/src/components/CompilerDropdown.js
@@ -0,0 +1,10 @@
+export default function CompilerDropdown() {
+  return (
+    <div>
+      <label>Compiler: </label>
+      <select>
+        <option>SAXON HE</option>
+      </select>
+    </div>
+  );
+}
