9444a468bdee99417c8f499066ac603ba176c29d
rameshvaranganti <ramesh.varanganti@gmail.com>
2025-12-02 20:01:46 -0500

    Add ParamsInput component for parameter input

---
+++ frontend/src/components/ParamsInput.js
@@ -0,0 +1,9 @@
+export default function ParamsInput() {
+  return (
+    <div>
+      <h3>Input Params</h3>
+      <input placeholder="Name" />
+      <input placeholder="Value" />
+    </div>
+  );
+}
